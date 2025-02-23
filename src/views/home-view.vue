3a64515f00f71c79b5fbfabb16aafd16aa54661d
Petrichor <572752189@qq.com>
2025-02-23 20:25:30 +0800

    chore: 初始化项目配置文件

---
+++ src/views/home-view.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="log">{{ msg }}</button>
+</template>
+<script setup lang="ts">
+// 变量
+const msg = 'Hello!'
+
+// 函数
+function log() {
+  console.log(msg)
+}
+</script>
